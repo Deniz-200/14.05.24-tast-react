1b6cc82955134f760f6ee3c076735fd8758e58fc
deniz-200 <ld5yugzy6@code.edu.az>
2024-05-14 12:24:29 +0400

    react

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,98 @@
+
+import './App.css'
+
+function App(){
+    return (
+
+      <div>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
+        <div className="container">
+        <nav className='navbar'>
+
+<img src="https://templatemo.com/templates/templatemo_574_mexant/assets/images/logo.png" alt="" className='img1'/>
+<ul>
+  <li>Home</li>
+  <li>Services</li>
+  <li>About</li>
+  <form>
+  <select id="pages" name="pages">
+  <option value="pages" >Pages</option>
+  <option value="au">About Us</option>
+  <option value="os">Our Services</option>
+  <option value="cu">Contact Us</option>
+  </select>
+</form>
+  <li>Testimonials</li>
+  <button className='btn1'>Contact Support</button>
+</ul>
+</nav>
+</div>
+<section className='secbos'></section>
+<section className='sec1'>
+  <div className='div1'>
+<div className='dd1'><i class="bi bi-archive-fill"></i></div>
+<div className='dd2'>
+  <h1>CSS Templates</h1>
+  <p>TemplateMo website is the best for you to explore and download free website templates.</p>
+</div>
+  </div>
+  <div className='div1'>
+<div className='dd1'><i class="bi bi-cloudy-fill"></i></div>
+<div className='dd2'>
+  <h1>HTML5 Web Pages</h1>
+  <p>Templates are based on Bootstrap 5 CSS framework. You can easily adapt or modify based on your needs.</p>
+</div>
+  </div>
+  <div className='div1'>
+<div className='dd1'><i class="bi bi-ev-station-fill"></i></div>
+<div className='dd2'>
+  <h1>Responsive Designs</h1>
+  <p>All of our CSS templates are 100% free to use for commercial or business websites.</p>
+</div>
+  </div>
+  <div className='div1'>
+<div className='dd1'><i class="bi bi-suitcase-lg-fill"></i></div>
+<div className='dd2'>
+  <h1>Mobile and Tablet ready!</h1>
+  <p>Our HTML CSS templates are well-tested on mobile, tablet, and desktop compatibility.</p>
+</div>
+</div>
+  <div className='div1'>
+<div className='dd1'><i class="bi bi-bank"></i></div>
+<div className='dd2'>
+  <h1>Fast Customer Support</h1>
+  <p>Do not be hesitated to contact us if you have any question or concern about our templates.</p>
+</div>
+  </div>
+ 
+  <div className='div1'>
+<div className='dd1'><i class="bi bi-puzzle-fill"></i></div>
+<div className='dd2'>
+  <h1>Fully Customizable</h1>
+  <p>If you have any idea or suggestion about new templates, feel free to let us know.</p>
+</div>
+  </div>
+</section>
+<section className='secbos'></section>
+<section className='sec3'>
+<div className='divphoto'><img src="https://templatemo.com/templates/templatemo_574_mexant/assets/images/calculator-image.png" alt="" className='imgw' /></div>
+<div className='divtext'>
+  <div className='divh'><h3> YOUR FREEDOM</h3>
+  <h1 className='texth'>Get A Financial Plan</h1></div>
+ <div className='container2'>
+  <div className='inpdiv'>
+   <div><p>Your Name</p> <input type="text" /></div>
+   <div><p>Your Email</p> <input type="text" /></div>
+  </div>
+  <div className='inpd'><p>Subject</p> <input type="text" /></div>
+  <div className='inpd'><p>Your Reason</p> <input type="text" /></div>
+  <button className='textbtn'>Submit Now</button>
+</div>
+</div>
+</section>
+      </div>
+    )
+  }
+
+
+export default App
